46e53b352e2c576a00ab6deb7231bb3095c96a39
BandiSamuel <73738137+BandiSamuel@users.noreply.github.com>
2021-11-09 07:43:55 +0530

    Update data_visualization_app.py

--- data_visualization_app.py
+++ data_visualization_app.py
@@ -1,9 +1,5 @@
 import streamlit as st
 import pandas as pd
-import dash
-import dash_core_components as dcc
-import dash_html_components as html
-from dash.dependencies import Input, Output
 import plotly.express as px
 
 # configuration
