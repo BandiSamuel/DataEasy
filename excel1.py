9fcf1686aac48db08dca9956c0cec96eee0f3c24
BandiSamuel <73738137+BandiSamuel@users.noreply.github.com>
2021-11-09 05:35:50 +0530

    Update excel1.py

--- excel1.py
+++ excel1.py
@@ -1,9 +1,12 @@
 import streamlit as st
 import pandas as pd
 
-uploaded_file = st.file_uploader("Choose a XLSX file", type="xlsx")
+file = st.file_uploader("Choose an excel file", type="xlsx")
 
-if uploaded_file:
-    df = pd.read_excel(uploaded_file)
-st.dataframe(df)
-st.table(df)
+if st.button('go!'):
+        
+    all_sheet = pd.ExcelFile(file)   
+    sheets = all_sheet.sheet_names
+
+    for i in range(len(sheets)):
+        df = pd.read_excel(file, sheet_name = sheets[i])
